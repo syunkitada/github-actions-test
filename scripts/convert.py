b0a296d6f1f09f34c40e9fbd65b3fcdf3cba520a
syunkitada <syun.kitada@gmail.com>
2024-07-07 00:25:08 +0900

    update

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-# import re
-# 
-# lines = input()
-# 
-# for line in lines.splitlines():
-#     result = re.findall(r'(.*):(\d+):(\d+): (.*)', line)[0]
-#     print(f"::error file={result[0]},line={result[1]},col={result[2]},title=flake8::{result[3]}")
+import re
+import sys
 
-print("::error file=app.py,line=1,col=5::Missing semicolon")
+lines = sys.stdin.readlines()
+
+for line in lines:
+    result = re.findall(r"(.*):(\d+):(\d+): (.*)", line)
+    if len(result) > 0:
+        result = result[0]
+        print(
+            f"::error file={result[0]},line={result[1]},col={result[2]},title=flake8::{result[3]}"
+        )
